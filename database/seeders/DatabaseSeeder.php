bf7abb04e1576423a355d4173a3c289b56111bdf
Md Ismail Hossain <ihsojib43@gmail.com>
2024-04-14 19:19:15 +0600

    added models migrations seeder observer

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Feature;
+use App\Models\Package;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -16,8 +18,43 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Ismail Hossain',
+            'email' => 'ismail@email.com',
+            'password' => bcrypt('123456789'),
+        ]);
+
+        Feature::create([
+            'image' => 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_light_color_272x92dp.png',
+            'route_name' => 'feature1.index',
+            'name' => 'BMI Calculator',
+            'description' => 'Calculate BMI index according to the information',
+            'required_credits' => 1,
+            'active' => true,
+        ]);
+
+        Feature::create([
+            'image' => 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_light_color_272x92dp.png',
+            'route_name' => 'feature1.index',
+            'name' => 'PPI Calculator',
+            'description' => 'Calculate PPI according to the information',
+            'required_credits' => 4,
+            'active' => true,
+        ]);
+
+        Package::create([
+            'name' => 'Basic',
+            'price' => '5',
+            'credits' => 20
+        ]);
+        Package::create([
+            'name' => 'Silver',
+            'price' => '20',
+            'credits' => 100
+        ]);
+        Package::create([
+            'name' => 'Golden',
+            'price' => '50',
+            'credits' => 500
         ]);
     }
 }
